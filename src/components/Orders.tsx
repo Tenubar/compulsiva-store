2ce11de2ff1d1b67f1aca4bcb36ca2766cc02254
Tenubar <tenubarr@gmail.com>
2025-04-30 21:01:27 -0400

    update

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -216,7 +216,7 @@ const Orders: React.FC = () => {
                   </div>
                 </div>
               ) : (
-                <div className="overflow-hidden">
+                <div className="overflow-auto">
                   <table className="min-w-full divide-y divide-gray-200">
                     <thead className="bg-gray-50">
                       <tr>
