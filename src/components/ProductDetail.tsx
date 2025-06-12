f799ed8f61587cd19291a6c4710e561d85b595f6
solarapptech <solartech4756@gmail.com>
2025-06-11 21:18:01 -0400

    1231231

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -2218,8 +2218,8 @@ const ProductDetail: React.FC<ProductDetailProps> = ({ onBack }): JSX.Element =>
           name="amount"
           value={
             selectedShipping && selectedShipping.price > 0
-              ? (priceToUse * quantity + selectedShipping.price).toFixed(2)
-              : (priceToUse * quantity).toFixed(2)
+              ? (priceToUse + selectedShipping.price).toFixed(2)
+              : (priceToUse).toFixed(2)
           }
         />
         <input type="hidden" name="quantity" value={quantity} />
